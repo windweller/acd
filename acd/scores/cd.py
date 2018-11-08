da102feca5a1d90d7a351943cf56789281e39868
windweller <leo.niecn@gmail.com>
2018-11-07 23:04:25 -0800

    add snli/dis score computation

--- acd/scores/cd.py
+++ acd/scores/cd.py
@@ -126,18 +126,18 @@ def cd(blob, im_torch, model, model_type='mnist'):
     return relevant, irrelevant
 
 def fast_cd_text(batch, model, start, stop):
+    # this "batch" actually gets changed by the acd code....ha
+    # not always the fixed
+
     weights = model.lstm.state_dict()
 
     # Index one = word vector (i) or hidden state (h), index two = gate
-    W_ii, W_if, W_ig, W_io = np.split(weights['weight_ih_l0'], 4, 0)
-    W_hi, W_hf, W_hg, W_ho = np.split(weights['weight_hh_l0'], 4, 0)
+    # W_ii, W_if, W_ig, W_io = np.split(weights['weight_ih_l0'], 4, 0)
+    # W_hi, W_hf, W_hg, W_ho = np.split(weights['weight_hh_l0'], 4, 0)
     b_i, b_f, b_g, b_o = np.split(weights['bias_ih_l0'].numpy() + weights['bias_hh_l0'].numpy(), 4)
 
-    W_x = weights['weight_ih_l0']  # h_dim x word_dim (512 x 300)
-    W_h = weights['weight_hh_l0']  # h_dim x
-
-    # TODO: 1. let's not resplit them.....since it's CLEARLY faster as one operation vs 4
-    # TODO:    do matrix operation together, then split for individual vectors
+    W_x = weights['weight_ih_l0']  # 4 * h_dim x word_dim (512 x 300)
+    W_h = weights['weight_hh_l0']  # 4 * h_dim x h_dim (512 x 128)
 
     word_vecs = model.embed(batch.text)[:, 0].data
     T = word_vecs.size(0)
@@ -153,25 +153,37 @@ def fast_cd_text(batch, model, start, stop):
             prev_rel_h = np.zeros(model.hidden_dim)
             prev_irrel_h = np.zeros(model.hidden_dim)
 
-        rel_i = np.dot(W_hi, prev_rel_h)
-        rel_g = np.dot(W_hg, prev_rel_h)
-        rel_f = np.dot(W_hf, prev_rel_h)
-        rel_o = np.dot(W_ho, prev_rel_h)
-        irrel_i = np.dot(W_hi, prev_irrel_h)
-        irrel_g = np.dot(W_hg, prev_irrel_h)
-        irrel_f = np.dot(W_hf, prev_irrel_h)
-        irrel_o = np.dot(W_ho, prev_irrel_h)
+        # rel_i = np.dot(W_hi, prev_rel_h)
+        # rel_g = np.dot(W_hg, prev_rel_h)
+        # rel_f = np.dot(W_hf, prev_rel_h)
+        # rel_o = np.dot(W_ho, prev_rel_h)
+
+        rel_gates = np.dot(W_h, prev_rel_h)
+        irrel_gates = np.dot(W_h, prev_irrel_h)
+
+        # irrel_i = np.dot(W_hi, prev_irrel_h)
+        # irrel_g = np.dot(W_hg, prev_irrel_h)
+        # irrel_f = np.dot(W_hf, prev_irrel_h)
+        # irrel_o = np.dot(W_ho, prev_irrel_h)
 
         if i >= start and i <= stop:
-            rel_i = rel_i + np.dot(W_ii, word_vecs[i])
-            rel_g = rel_g + np.dot(W_ig, word_vecs[i])
-            rel_f = rel_f + np.dot(W_if, word_vecs[i])
-            rel_o = rel_o + np.dot(W_io, word_vecs[i])
+            # rel_i = rel_i + np.dot(W_ii, word_vecs[i])
+            # rel_g = rel_g + np.dot(W_ig, word_vecs[i])
+            # rel_f = rel_f + np.dot(W_if, word_vecs[i])
+            # rel_o = rel_o + np.dot(W_io, word_vecs[i])
+
+            rel_gates += np.dot(W_x, word_vecs[i])
+
         else:
-            irrel_i = irrel_i + np.dot(W_ii, word_vecs[i])
-            irrel_g = irrel_g + np.dot(W_ig, word_vecs[i])
-            irrel_f = irrel_f + np.dot(W_if, word_vecs[i])
-            irrel_o = irrel_o + np.dot(W_io, word_vecs[i])
+            # irrel_i = irrel_i + np.dot(W_ii, word_vecs[i])
+            # irrel_g = irrel_g + np.dot(W_ig, word_vecs[i])
+            # irrel_f = irrel_f + np.dot(W_if, word_vecs[i])
+            # irrel_o = irrel_o + np.dot(W_io, word_vecs[i])
+
+            irrel_gates += np.dot(W_x, word_vecs[i])
+
+        rel_i, rel_g, rel_f, rel_o = np.split(rel_gates, 4, 0)
+        irrel_i, irrel_g, irrel_f, irrel_o = np.split(irrel_gates, 4, 0)
 
         rel_contrib_i, irrel_contrib_i, bias_contrib_i = propagate_three(rel_i, irrel_i, b_i, sigmoid)
         rel_contrib_g, irrel_contrib_g, bias_contrib_g = propagate_three(rel_g, irrel_g, b_g, np.tanh)
@@ -281,6 +293,124 @@ def cd_text(batch, model, start, stop):
 
     return scores
 
+def snli_model_process(batch, model, start, stop, premise=True):
+    # batch = x / input in your code!!!
+
+    input_premise, length_premise = batch.premise
+    input_hypothesis, length_hypothesis = batch.hypothesis
+
+    embed_premise = model.embed(input_premise)
+    embed_hypothesis = model.embed(input_hypothesis)
+    output_premise = model.autolen_rnn(embed_premise, length_premise)
+    output_hypothesis = model.autolen_rnn(embed_hypothesis, length_hypothesis)
+
+    # we get relevant vector from LSTM
+    # for SNLI
+    fc1_W = model.fc1.weight.detach().numpy()
+    fc1_b = model.fc1.bias.detach().numpy()
+    out_W = model.out.weight.detach().numpy()
+    out_b = model.out.bias.detach().numpy()
+
+
+    if premise:
+        rel_premise = snli_cd_text(embed_premise, model, start, stop)
+        rel_concat = torch.cat([rel_premise, output_hypothesis,
+                                rel_premise - output_hypothesis,
+                                rel_premise * output_hypothesis], dim=1)
+        # scores for each label
+        # TODO: for Yuhui, fix the dimension!!!
+        scores = np.dot(np.dot(fc1_W, rel_concat) + fc1_b, out_W) + out_b
+    else:
+        rel_hyp = snli_cd_text(embed_hypothesis, model, start, stop)
+        rel_concat = torch.cat([output_premise, rel_hyp,
+                                output_premise - rel_hyp,
+                                output_premise * rel_hyp], dim=1)
+        # scores for each label
+        # TODO: for Yuhui, fix the dimension!!!
+        scores = np.dot(np.dot(fc1_W, rel_concat) + fc1_b, out_W) + out_b
+
+    return scores
+
+
+def snli_cd_text(sentence, model, start, stop):
+    weights = model.encoder.state_dict()
+
+    # Index one = word vector (i) or hidden state (h), index two = gate
+    W_ii, W_if, W_ig, W_io = np.split(weights['weight_ih_l0'], 4, 0)
+    W_hi, W_hf, W_hg, W_ho = np.split(weights['weight_hh_l0'], 4, 0)
+    b_i, b_f, b_g, b_o = np.split(weights['bias_ih_l0'].numpy() + weights['bias_hh_l0'].numpy(), 4)
+    # word_vecs = model.embed(batch.text)[:, 0].data
+
+    # TODO: fix the dimension here!!!
+    word_vecs = sentence[:,0].data
+
+    T = word_vecs.size(0)
+    relevant = np.zeros((T, model.hidden_dim))
+    irrelevant = np.zeros((T, model.hidden_dim))
+    relevant_h = np.zeros((T, model.hidden_dim))
+    irrelevant_h = np.zeros((T, model.hidden_dim))
+    for i in range(T):
+        if i > 0:
+            prev_rel_h = relevant_h[i - 1]
+            prev_irrel_h = irrelevant_h[i - 1]
+        else:
+            prev_rel_h = np.zeros(model.hidden_dim)
+            prev_irrel_h = np.zeros(model.hidden_dim)
+
+        rel_i = np.dot(W_hi, prev_rel_h)
+        rel_g = np.dot(W_hg, prev_rel_h)
+        rel_f = np.dot(W_hf, prev_rel_h)
+        rel_o = np.dot(W_ho, prev_rel_h)
+        irrel_i = np.dot(W_hi, prev_irrel_h)
+        irrel_g = np.dot(W_hg, prev_irrel_h)
+        irrel_f = np.dot(W_hf, prev_irrel_h)
+        irrel_o = np.dot(W_ho, prev_irrel_h)
+
+        if i >= start and i <= stop:
+            rel_i = rel_i + np.dot(W_ii, word_vecs[i])
+            rel_g = rel_g + np.dot(W_ig, word_vecs[i])
+            rel_f = rel_f + np.dot(W_if, word_vecs[i])
+            rel_o = rel_o + np.dot(W_io, word_vecs[i])
+        else:
+            irrel_i = irrel_i + np.dot(W_ii, word_vecs[i])
+            irrel_g = irrel_g + np.dot(W_ig, word_vecs[i])
+            irrel_f = irrel_f + np.dot(W_if, word_vecs[i])
+            irrel_o = irrel_o + np.dot(W_io, word_vecs[i])
+
+        rel_contrib_i, irrel_contrib_i, bias_contrib_i = propagate_three(rel_i, irrel_i, b_i, sigmoid)
+        rel_contrib_g, irrel_contrib_g, bias_contrib_g = propagate_three(rel_g, irrel_g, b_g, np.tanh)
+
+        relevant[i] = rel_contrib_i * (rel_contrib_g + bias_contrib_g) + bias_contrib_i * rel_contrib_g
+        irrelevant[i] = irrel_contrib_i * (rel_contrib_g + irrel_contrib_g + bias_contrib_g) + (rel_contrib_i + bias_contrib_i) * irrel_contrib_g
+
+        if i >= start and i < stop:
+            relevant[i] += bias_contrib_i * bias_contrib_g
+        else:
+            irrelevant[i] += bias_contrib_i * bias_contrib_g
+
+        if i > 0:
+            rel_contrib_f, irrel_contrib_f, bias_contrib_f = propagate_three(rel_f, irrel_f, b_f, sigmoid)
+            relevant[i] += (rel_contrib_f + bias_contrib_f) * relevant[i - 1]
+            irrelevant[i] += (rel_contrib_f + irrel_contrib_f + bias_contrib_f) * irrelevant[i - 1] + irrel_contrib_f * \
+                                                                                                      relevant[i - 1]
+
+        o = sigmoid(np.dot(W_io, word_vecs[i]) + np.dot(W_ho, prev_rel_h + prev_irrel_h) + b_o)
+        rel_contrib_o, irrel_contrib_o, bias_contrib_o = propagate_three(rel_o, irrel_o, b_o, sigmoid)
+        new_rel_h, new_irrel_h = propagate_tanh_two(relevant[i], irrelevant[i])
+        # relevant_h[i] = new_rel_h * (rel_contrib_o + bias_contrib_o)
+        # irrelevant_h[i] = new_rel_h * (irrel_contrib_o) + new_irrel_h * (rel_contrib_o + irrel_contrib_o + bias_contrib_o)
+        relevant_h[i] = o * new_rel_h
+        irrelevant_h[i] = o * new_irrel_h
+
+    # W_out = model.hidden_to_label.weight.data
+
+    # Sanity check: scores + irrel_scores should equal the LSTM's output minus model.hidden_to_label.bias
+    # scores = np.dot(W_out, relevant_h[T - 1])
+    # irrel_scores = np.dot(W_out, irrelevant_h[T - 1])
+
+    # rel_concatenate
+
+    return relevant_h[T - 1]
 
 
 # get contextual decomposition scores for blob
